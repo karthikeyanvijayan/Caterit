5677c0b38ba0aa374626e1a50095d7bf6ae00293
Karthikeyan <karthikeyan.vjn@gmail.com>
2017-01-31 12:15:15 +0530

    Saving customer info in sharedpreferences Signup / signing textfield data

--- Caterit/app/src/main/java/karthik/com/caterit/Fragments/SigninFragment.java
+++ Caterit/app/src/main/java/karthik/com/caterit/Fragments/SigninFragment.java
@@ -5,10 +5,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import karthik.com.caterit.R;
 
 public class SigninFragment extends Fragment {
+
+    EditText textEmail,textPassword;
+
+    Button btnSignin;
 
 
     public SigninFragment() {
@@ -20,6 +26,20 @@ public class SigninFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signin, container, false);
+        View view = inflater.inflate(R.layout.fragment_signin, container, false);
+
+        textEmail = (EditText) view.findViewById(R.id.textLoginUsername);
+        textPassword = (EditText) view.findViewById(R.id.textLoginPassword);
+
+        btnSignin = (Button) view.findViewById(R.id.buttonSignin);
+        btnSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        return  view;
     }
 }
--- Caterit/app/src/main/java/karthik/com/caterit/Fragments/SignupFragment.java
+++ Caterit/app/src/main/java/karthik/com/caterit/Fragments/SignupFragment.java
@@ -1,18 +1,31 @@
 package karthik.com.caterit.Fragments;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.gson.Gson;
+
+import karthik.com.caterit.Models.Customer;
 import karthik.com.caterit.R;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SignupFragment extends Fragment {
+
+    EditText textName, textEmail, textMobile, textPassword;
+
+    Button btnSignup;
 
 
     public SignupFragment() {
@@ -23,8 +36,43 @@ public class SignupFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false);
+
+        View view = (View) inflater.inflate(R.layout.fragment_signup, container, false);
+
+        final SharedPreferences mPrefs = this.getActivity().getPreferences(MODE_PRIVATE);
+
+        textEmail = (EditText) view.findViewById(R.id.textSgEmail);
+        textName = (EditText) view.findViewById(R.id.textSgName);
+        textMobile = (EditText) view.findViewById(R.id.textSgMobile);
+        textPassword = (EditText) view.findViewById(R.id.textSgPassword);
+
+        btnSignup = (Button) view.findViewById(R.id.buttonSignup);
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Customer customer = new Customer();
+                customer.setName(textName.getText().toString());
+                customer.setPassword(textPassword.getText().toString());
+                customer.setEmail(textEmail.getText().toString());
+                customer.setMobile(textMobile.getText().toString());
+
+                SharedPreferences.Editor mEditor = mPrefs.edit();
+
+                Gson gson = new Gson();
+                String customerJson = gson.toJson(customer);
+
+                mEditor.putString(getString(R.string.pref_customer), customerJson);
+                mEditor.commit();
+
+                String customerInfoString = mPrefs.getString(getString(R.string.pref_customer), "");
+                Customer savedCustomer = gson.fromJson(customerInfoString, Customer.class);
+                Log.d("customer", savedCustomer.toString());
+
+            }
+        });
+
+        return view;
     }
 
 }
